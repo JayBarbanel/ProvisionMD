dcf407ff1b001540df7311f05c1787ec873cbec2
Jay <jbarbanel531@gmail.com>
2022-02-18 15:31:33 -0800

    added text over image, will design later

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,13 +8,19 @@ import{
 function Main (){
     return (
         <Card>
-        <Card.Img variant="top" src={main} />
+            <div className="head-text">
+        <Card.Img variant="top" src={main}/>
+        <div className="text-on-image">
+        <h1 className="welcome">Welcome!</h1>
+        <p className="text-one">The Simplest Way to Saftley and Easily Refll Your Subscriptions.</p>
+        </div>
         <Card.Body>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk
             of the card's content.
           </Card.Text>
         </Card.Body>
+        </div>
       </Card>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,12 +11,12 @@ function NavBar (){
     return(
         <Navbar bg="light" expand="md">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">ProvisionMD</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#home">Refill Subsription</Nav.Link>
+              <Nav.Link href="#link">Contact A Doctor</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
