67ffad95c2123be591c9863e60489178d9a2343d
Jay <jbarbanel531@gmail.com>
2022-02-18 13:14:49 -0800

    updated files, added components folder, added nabar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css';
+import NavBar from './components/Nav'
 import { BrowserRouter as Router} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <Router>
     <div className="App">
-   "Hello World"
+   <NavBar/>
     </div>
 
     </Router>
