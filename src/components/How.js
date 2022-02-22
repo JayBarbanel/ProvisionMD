a0b7148abdb7be2f7928acca113c61e3f34a129e
Jay <jbarbanel531@gmail.com>
2022-02-22 13:38:22 -0800

    updated the who we are section, added the steps to take header

--- src/components/How.js
+++ src/components/How.js
@@ -13,22 +13,26 @@ function How (){
   <Card>
     <Card.Body>
       <Card.Title>
-          <div className="how-to">
-              <h2>How It Works</h2>
+          <div className="who-we">
+              <h2>Who We Are</h2>
           </div>
       </Card.Title>
       <Card.Text>
-          <div className="instruction">
-        When you're in a rush and need to fill your perscription, <br/>
-        being on hold can be a pain.  With ProvisionMD, all you need to do is <br/>
-        sign up with your doctor, create an account and re-order online!
+          <div className="wwa">
+        Anything that needs medical attnetion can be stressfull, inuding ordering perscriptions.<br/>
+        At ProvisionsMD, our goal is to make the experiance easier and less of a hassle. <br/>
+        One approved by your doctor and your insurance company, all you have to do is sign in, <br/>
+        check off the medications you want to refill, and then we take care of the rest.
         </div>
-        <div>
-          <ul>
-            <li>Talk to your doctor about siging up.</li>
-            <li>Easily run all financials through your insurance.</li>
-            <li>Get online, sign in and have refill your medications.</li>
-            <li>Your order will be delivered right to your home.</li>
+        <div id="steps">
+        <h3>Steps to take</h3>
+        </div>
+        <div className="list">
+          <ul id="list">
+            <li>Talk to your doctor about siging up.</li> <br/>
+            <li>Easily run all financials through your insurance.</li><br/>
+            <li>Get online, sign in and have refill your medications.</li><br/>
+            <li>Your order will be delivered right to your home.</li><br/>
           </ul>
         </div>
       </Card.Text>
